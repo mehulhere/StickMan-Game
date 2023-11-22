8b28f8f91a50b7150b95a9b331c34ceef0819400
Mehul Pahuja <99717294+mehulhere@users.noreply.github.com>
2023-11-23 01:52:32 +0530

    Delete Desktop/Firstgame directory

--- Desktop/Firstgame/src/main/java/com/example/first_fx_project/DefaultCharacter.java
+++
@@ -1,9 +0,0 @@
-package com.example.first_fx_project;
-
-public class DefaultCharacter extends Player{
-    static private int maxMeter;
-
-    public DefaultCharacter(int length, int width, int speed, boolean isInverted, boolean meterIsFull) {
-        super(length, width, speed, isInverted, meterIsFull);
-    }
-}
--- Desktop/Firstgame/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.first_fx_project {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.first_fx_project to javafx.fxml;
-    exports com.example.first_fx_project;
-}

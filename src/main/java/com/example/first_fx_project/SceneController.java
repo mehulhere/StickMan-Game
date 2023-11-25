657c1db673a7daab7e6946b7455b4344f103771a
mehulhere <themehulpahuja@gmail.com>
2023-11-26 03:41:25 +0530

    Aditya UI Upgrades

--- src/main/java/com/example/first_fx_project/SceneController.java
+++ src/main/java/com/example/first_fx_project/SceneController.java
@@ -5,12 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class SceneController {
+    public ImageView imgDefaultCharacter;
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -45,14 +47,13 @@ public class SceneController {
     }
 
 
+
     public void switchToCharacterPage(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("gamePlayPage.fxml"));
-        Parent root = fxmlLoader.load();
-
-        GamePlayController controller = fxmlLoader.getController(); // Get the controller instance
-
-        stage.setTitle("Game Play Page");
-        stage.setScene(new Scene(root, 800, 600));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("characterPage.fxml"));
+        root = fxmlLoader.load(); // Set the loaded FXML as the root
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
         stage.show();
     }
 
@@ -67,5 +68,9 @@ public class SceneController {
 
     public void exitGame(ActionEvent event) throws IOException {
         System.exit(0);
+    }
+
+
+    public void directionButtonClick(ActionEvent actionEvent) {
     }
 }
